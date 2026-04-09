5ecfa85e207e48cb5c50f6ec96ca270054db114a
hio0 <hioohio1234@naver.com>
2026-04-09 20:02:33 +0900

    d

--- AGK02/Assets/3. Scripts/DictionaryPractice.cs
+++ AGK02/Assets/3. Scripts/DictionaryPractice.cs
@@ -4,59 +4,100 @@ using UnityEngine;
 
 public class DictionaryPractice : MonoBehaviour
 {
-    public Dictionary<string,int> lb = new Dictionary<string, int>();
+    public Dictionary<string, int> lb = new Dictionary<string, int>();
 
-    public string bililbook;
+    string manyitem;
+    int allItem;
 
-    public bool isbannap;
-    public bool isit;
+    public string what;
+    public bool get;
+    public bool use;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        lb.Add("걱걱걱걱", 1);
-        lb.Add("겍겍겍겍", 2);
-        lb.Add("곅곅곅곅", 3);
-        /*
-        lb["해리포터"] = 1;
+        lb.Add("화살", 1);
+        lb.Add("돌맹이", 3);
+        lb.Add("물약", 5);
 
-        foreach(KeyValuePair<string,int> book in lb) // string, int dictionary 중첩 형식의 book 변수로 lb를 넣음.
-        {
-            Debug.Log("책 제목" + book.Key + "재고" + book.Value);
-        }
-
-        if(lb.ContainsKey("걱걱걱걱"))
-        {
-            Debug.Log("걱걱걱걱의 현재 재고: " + lb["걱걱걱걱"]);
-        }
-
-        lb["걱걱걱걱"]++;
-        */
-        foreach (KeyValuePair<string, int> book in lb) // foreach는 for과는 달리 무조건 대상 리스트의 처음부터 끝까지 반복.
-        {
-            if (book.Key == bililbook && book.Value > 0)
-            {
-                isit = true;
-            }
-        }
-
-        if(isit)
-        {
-            Debug.Log($"{bililbook}을 대출합니다.");
-            lb[bililbook]--;
-            Debug.Log($"{bililbook}의 대출을 완료하였습니다.");
-        }
+        ItemConsole();
+        ShowAllIB();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isbannap)
+        if (get)
         {
-            lb[bililbook]++;
-            Debug.Log($"{bililbook}의 반납을 완료하였습니다.");
-            isbannap = false;
+            lb[what]++;
+
+            int i = lb[what];
+            Debug.Log($"{what} 획득(현재 수량: {i})");
+
+            get = false;
+        }
+
+        if (use)
+        {
+            bool isit = false;
+            bool isNoZero = true;
+            foreach (KeyValuePair<string, int> book in lb) 
+            {
+                if (book.Key == what)
+                {
+                   isit = true;
+                    if(book.Value <= 0)
+                    {
+                        isNoZero = false;
+                    }
+                }
+            }
+
+            if(isNoZero)
+            {
+                if (isit)
+                {
+                    lb[what]--;
+
+                    int i = lb[what];
+                    Debug.Log($"{what} 사용 완료(남은 수량: {i})");
+                }
+                else
+                {
+                    Debug.Log($"{what}은 존재하지 않는 아이템입니다;;; ");
+                }
+            }
+            else
+            {
+                Debug.Log($"{what}의 수량이 부족합니다");
+            }
+            use = false;
+        }
+    }
+
+    void ItemConsole()
+    {
+        int many = 0;
+
+        foreach (KeyValuePair<string, int> book in lb) // foreach는 for과는 달리 무조건 대상 리스트의 처음부터 끝까지 반복.
+        {
+            if (book.Value > many)
+            {
+                many = book.Value;
+                manyitem = book.Key;
+            }
+
+            allItem += book.Value;
+        }
+        Debug.Log($"가장 많은 아이템:{manyitem}");
+        Debug.Log($"총 아이템 개수:{allItem}");
+    }
+
+    void ShowAllIB()
+    {
+        foreach (KeyValuePair<string, int> book in lb) 
+        {
+            Debug.Log($"{book.Key} x{book.Value}");
         }
     }
 }
